5713c1808600b99aa60c6f4fc7813546bd9c0fe6
2002五月女 <94036178+2002saotome@users.noreply.github.com>
2024-05-31 16:20:50 +0900

    シーン管理(途中)

---
+++ Assets/toa/Script/ChangeExplanation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeExplanation : MonoBehaviour
+{
+    void Update()
+    {
+        // Spaceキー押すとシーン移行
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // SampleSceneに切り替える
+            SceneManager.LoadScene("ExplanationScene");
+        }
+    }
+}
