2ba9a9a9002f2d4d7375267b51a39cdca0600fae
ShiTokuon <k021g1160@g.neec.ac.jp>
2024-08-04 23:51:31 +0900

    調整

--- Assets/Script/ExpOrbScript.cs
+++ Assets/Script/ExpOrbScript.cs
@@ -22,7 +22,7 @@ public class ExpOrbScript : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            AudioManager.Instance.PlaySE("poka", 0.8f);
+            AudioManager.Instance.PlaySE("poka", 0.5f);
             Exp++;
             ExpText.text = Exp.ToString();
             ExpManeger.instance.ExpBarDraw();//経験値を拾った時に経験値バーの描画を更新する
